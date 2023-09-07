781158c58555218fb21ba8d6adfc50b3c74c01f9
Federico M <federico.mantovani2001@gmail.com>
2023-09-07 19:27:11 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
-fetch("./artists/artists.json")
-        .then((res) => {
-        return res.json();
-    })
-    .then((data) => console.log(data));
+window.onload = loadJSON;
+let artists = null;
+
+async function loadJSON() {
+    const response = await fetch("./artists/artists.json");
+    artists = await response.json();
+    console.log(artists);
+}
