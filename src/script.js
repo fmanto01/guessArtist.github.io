e12d51eaa9d57ef8ec83411efd0dafbdc67c2e0d
Federico M <federico.mantovani2001@gmail.com>
2023-09-07 19:06:16 +0200

    Added JSON file

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-alert("Hello");
+fetch('./artists/artists.json')
+  .then(response => response.json())
+  .then(data => {
+    // Ora il contenuto del file JSON è stato estratto e è disponibile nella variabile 'data'.
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Errore nel caricamento del file JSON', error);
+  });
