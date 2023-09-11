69c7660e8f21eb8119cf788b9e424edc1bb0e25e
Federico M <federico.mantovani2001@gmail.com>
2023-09-11 11:47:58 +0200

    ?

--- src/script.js
+++ src/script.js
@@ -161,7 +161,3 @@ function getFlag(nationality) {
     let flag = '<i class="flag-icon flag-icon-' + nationality + '"></i>';
     return flag;
 }
-
-<a href="#" data-toggle="modal" data-target="#howToPlayModal">
-    <i class="fas fa-question-circle ml-2" style="font-size: 20px;"></i>
-</a>
